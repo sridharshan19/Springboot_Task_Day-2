0afdad350f2c2598370cef23311b8e762d1a80f0
sridharshan19 <153419884+sridharshan19@users.noreply.github.com>
2025-07-03 21:07:44 +0530

    Add files via upload
    Task-Student-Employee Spring Boot Application
    
    This is a basic Spring Boot application that manages student and employee data using Java-based services and models. It demonstrates core concepts such as:
    
    Spring Boot service layers
    
    Java class modeling 
    
    RESTful structure 
    
    Spring Data JDBC/MySQL integration

---
+++ Springbootfirst/src/main/java/com/example/Springbootfirst/SpringbootfirstApplication.java
@@ -0,0 +1,14 @@
+package com.example.Springbootfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfirstApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootfirstApplication.class, args);
+	}
+
+}
---
+++ Springbootfirst/src/main/java/com/example/Springbootfirst/models/Employee.java
@@ -0,0 +1,16 @@
+package com.example.Springbootfirst.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Employee
+{
+    private int empid;
+    private String name;
+    private String job;
+
+
+
+}
